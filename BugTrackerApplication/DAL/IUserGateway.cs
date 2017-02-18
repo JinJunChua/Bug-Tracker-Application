fba498fdf0cb2c1cbe22db8bda98da9eb6324b3e
JinJunChua <friedpuddingy@gmail.com>
2017-02-18 20:30:37 +0800

    removed DataGateway and added a DAL for user login

---
+++ BugTrackerApplication/DAL/IUserGateway.cs
@@ -0,0 +1,14 @@
+﻿using BugTrackerApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTrackerApplication.DAL
+{
+    interface IUserGateway<User> where User : class
+    {
+        User Login(string username, string password);
+    }
+}
--- BugTrackerApplication/DAL/UserGateway.cs
+++ BugTrackerApplication/DAL/UserGateway.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using BugTrackerApplication.Models;
+using System.Data.Entity;
 
 namespace BugTrackerApplication.DAL
 {
-    public class UserGateway : DataGateway<User>
+    public class UserGateway : IUserGateway<User>
     {
+        internal BugTrackerApplicationContext db = new BugTrackerApplicationContext();
+        internal DbSet<User> data = null;
+
+        public UserGateway()
+        {
+            this.data = db.Set<User>();
+        }
+
         public User Login(string username, string password)
         {
             var newObj = db.User.Where(x => x.userName.Equals(username) && x.password == password).FirstOrDefault();
