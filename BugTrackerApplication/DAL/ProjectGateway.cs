09eea1920c4773ca35080e546eb1e522131265f4
eddyalex <Eddy.Yeo_2015@SIT.SingaporeTech.edu.sg>
2017-03-30 18:59:00 +0800

    v3 BugTracker

--- BugTrackerApplication/DAL/ProjectGateway.cs
+++ BugTrackerApplication/DAL/ProjectGateway.cs
@@ -11,11 +11,12 @@ namespace BugTrackerApplication.DAL
     {
         internal DbSet<Project> projectdata = null;
         private IEnumerable<Project> allCaseByName;
-
+       // public DbSet<Project> Context;
 
         public ProjectGateway()
         {
             projectdata = db.Set<Project>();
+           // Context = db.Set<Project>();
         }
 
         //PM > Index
@@ -24,6 +25,7 @@ namespace BugTrackerApplication.DAL
             var displaysProjectThatUserHas = projectdata.Where(x => x.Manager.userID == id);            
             return displaysProjectThatUserHas; 
         }
+
 
         //PM > Index > Details > Cases (list of cases)
         public IEnumerable<Case> getListOfCases(int pid)
@@ -34,6 +36,22 @@ namespace BugTrackerApplication.DAL
             else
                 return null;
         }
+
+
+        //public IQueryable<Project> GetProducts(ProductSearchModel searchModel)
+        //{
+        //    var result = Context..Project.AsQueryable();
+        //    if (searchModel != null)
+        //    {
+        //        if (!string.IsNullOrEmpty(searchModel.projectName))
+        //            result = result.Where(x => x.Name.Contains(searchModel.projectName));
+        //        if (!string.IsNullOrEmpty(searchModel.projectDesc))
+        //            result = result.Where(x => x.Name.Contains(searchModel.projectDesc));
+        //        if (!string.IsNullOrEmpty(searchModel.projectVersion))
+        //            result = result.Where(x => x.Name.Contains(searchModel.projectVersion));
+        //    }
+        //    return result;
+        //}
 
 
         //for all other users
@@ -50,13 +68,14 @@ namespace BugTrackerApplication.DAL
             if (string.IsNullOrEmpty(searchTerm))
             {
 
-                return allCaseByName = projectdata.Where(x => x.projectDesc == searchTerm).ToList();
-
+                return allCaseByName = projectdata.Where(x => x.projectDesc == searchTerm).ToList();   
             }
             else
             {
                 //return allCaseByName = casedata.Where(x => x.status.StartsWith(searchTerm)).ToList(); 
-                return allCaseByName = projectdata.Where(x => x.projectDesc.ToLower().Contains(searchTerm.ToLower())).ToList();
+                //return allCaseByName = projectdata.Where(x => x.projectDesc.ToLower().Contains(searchTerm.ToLower())).ToList();
+                return allCaseByName = projectdata.Where(x => x.projectName.ToLower().Contains(searchTerm.ToLower())).ToList();
+                 
             }
 
         }
